c8c3b95be9a1c65498b00b454b24add25c9a5c83
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-13 15:45:12 -0400

    init commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }   
+
+        public void Print()
+        {
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Year: {Year}");
+        }
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿//Exercise 1:
+//Create a new C# .Net Core Console App
+//Create a class named Car - make sure it is public
+//In the Car class:
+//Create a Make property of type string that is public
+//Create a Model property of type string that is public
+//Create a Year property of type int that is public
+//In the Main method:
+//Create a new instance of the Car class
+//Set values in the properties for the object
+//Print the values of each property to the Console
+
+
+using ClassesExercise; 
+
+var car1 = new Car();
+car1.Make = "Hyundai";
+car1.Model = "Genesis";
+car1.Year = 2022;
+
+car1.Print();
+
+var car2 = new Car();
+car2.Make = "BMW";
+car2.Model = "5 Series";
+car2.Year = 2021;
+
+car2.Print();
